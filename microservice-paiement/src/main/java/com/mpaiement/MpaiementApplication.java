07b1c21ecbe7c239a61cd1e5111a553533f09052
Tabet <b_tabet@esi.dz>
2021-03-23 00:06:03 +0100

    Src et main mis à jours

--- microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
+++ microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MpaiementApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MpaiementApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(MpaiementApplication.class, args);
+	}
 }
